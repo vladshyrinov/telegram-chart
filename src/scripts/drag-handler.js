f1c938383aa1e787b218c1682d379e110f9c7d7a
Vladyslav Shyrinov <vladyslav.shyrinov@verivox.com>
2019-03-19 16:59:10 +0100

    right side event

--- src/scripts/drag-handler.js
+++ src/scripts/drag-handler.js
@@ -3,155 +3,72 @@ const windowSizer = document.getElementById('window-sizer');
 const sizerRight = document.querySelector('.sizer-right');
 const sizerLeft = document.querySelector('.sizer-left');
 
+windowSizer.onmousedown = (event) => {
+    event.preventDefault();
 
-// let clientX;
-// let offsetX;
+    const shiftXLeft = event.clientX - windowSizer.getBoundingClientRect().left;
+    const shiftXRight = windowSizer.getBoundingClientRect().right - event.clientX; 
+    const target = event.target;
 
-// function moveAt(e) {
-//     const target = e.target;
+    const onMouseMove = (event) => {
+        if (target === windowSizer) {
+            let newLeft = event.clientX - shiftXLeft - windowSizerWrapper.getBoundingClientRect().left;
 
-//     const windowSizerWrapperWidth = windowSizerWrapper.clientWidth;
-//     const windowSizerWidth = windowSizer.clientWidth;
-//     const distanceToMove = windowSizerWrapperWidth - windowSizerWidth;
-
-//     if (target === windowSizer) {
-//         console.log('windowSizer');
-//         if(e.clientX > clientX) {
-//             if(windowSizer.offsetLeft < distanceToMove) {
-//                 windowSizer.style.left = e.pageX + 10 + 'px';
-//             }
-//             clientX = e.clientX;
-//         } else {
-//             if (windowSizer.offsetLeft > 0) {
-//                 windowSizer.style.left = e.pageX - 10 + 'px';
-//             }
-//             clientX = e.clientX;
-//         }
-//     } else if (target === sizerRight) {
-//         console.log('sizerRight');
-//         // if (e.clientX > clientX && windowSizerWidth <= windowSizerWrapperWidth) {
-//         //     console.log('if');
-//         //     windowSizer.style.width = windowSizerWidth + (e.clientX - windowSizerWidth - 20) + 'px';
-//         // } else if (e.clientX < clientX && windowSizerWidth >= 100) {
-//         //     // windowSizer.style.width = windowSizerWidth - (e.clientX - windowSizerWidth - 20);
-//         // }
-//     } else {
-//         console.log('sizerLeft');
-//     }
-
-// }
-
-
-
-
-
-
-// let startEvent;
-// const windowSizerWrapperWidth = windowSizerWrapper.clientWidth;
-// const windowSizerWrapperCoords = windowSizerWrapper.getBoundingClientRect();
-// let windowSizerWidth = windowSizer.clientWidth;
-// let distanceToMove = windowSizerWrapperWidth - windowSizerWidth;
-
-
-// const onSizerMove = (e) => {
-//     // const target = startEvent.target;
-//     // const offsetX = startEvent.offsetX;
-//     // const pageX = startEvent.pageX;
+            if (newLeft < 0) {
+                newLeft = 0;
+            }
     
-//     if (target === windowSizer) {
-//         console.log('windowSizer');
-//         const windowSizerCoords = windowSizer.getBoundingClientRect();
-//         // if (e.pageX - offsetX + windowSizerCoords.width <= windowSizerWrapperCoords.width &&
-//         //     e.pageX - offsetX + windowSizerWrapperCoords.left >= windowSizerWrapperCoords.left) {
-//         //     windowSizer.style.left = (e.pageX - offsetX) + 'px';
-//         // } else {
-//         //     if ((windowSizerWrapperCoords.right - windowSizerCoords.right) <= (windowSizerWidth / 25) && e.pageX > pageX) {
-//         //         console.log('work right');
-//         //         windowSizer.style.left = windowSizerWrapperCoords.width - windowSizerCoords.width + 'px';
-//         //     }
+            const rightEdge = windowSizerWrapper.offsetWidth - windowSizer.offsetWidth;
+            
+            if (newLeft > rightEdge) {
+                newLeft = rightEdge;
+            }
+    
+            windowSizer.style.left = newLeft + 'px';
+        } else if (target === sizerRight) {
+            let newWidth = event.clientX - windowSizer.getBoundingClientRect().left + shiftXRight;
 
-//         //     if ((windowSizerCoords.left - windowSizerWrapperCoords.left) <= (windowSizerWidth / 25) && e.pageX < pageX) {
-//         //         console.log('work left');
-//         //         windowSizer.style.left = '0px';
-//         //     }
-//         // }
+            const widthMiminum = windowSizerWrapper.getBoundingClientRect().width / 10;
 
+            if (newWidth < widthMiminum) {
+                newWidth = widthMiminum;
+            }
 
-//     } else if (target === sizerRight) {
-//         console.log('sizerRight');
-//     } else {
-//         console.log('sizerLeft');
-//     }
-// }
+            const maxWidth = windowSizerWrapper.getBoundingClientRect().width - (windowSizer.getBoundingClientRect().left - windowSizerWrapper.getBoundingClientRect().left);
 
-// const onSizerUp = (e) => {
-//     console.log('mouseup event', e);
+            if (newWidth > maxWidth) {
+                newWidth = maxWidth;
+            }
 
-//     document.removeEventListener('mousemove', onSizerMove);
-//     document.removeEventListener('mouseup', onSizerUp);
-//     // windowSizer.removeEventListener('mouseleave', onSizerUp);
-// }
+            windowSizer.style.width = newWidth + 'px';
+        } else {
+            const previousLeft = windowSizer.getBoundingClientRect().left;
+            windowSizer.style.left = event.clientX - shiftXLeft - windowSizerWrapper.getBoundingClientRect().left + 'px';
+            const currentLeft = windowSizer.getBoundingClientRect().left;
+            const leftDifference = (currentLeft - previousLeft);
+            windowSizer.style.width = windowSizer.getBoundingClientRect().width - leftDifference + 'px';
 
-// const onSizerStart = (e) => {
-//     e.preventDefault();
-//     console.log('mousedown event', e);
-
-//     startEvent = e;
-
-//     document.addEventListener('mousemove', onSizerMove);
-//     document.addEventListener('mouseup', onSizerUp);
-//     // windowSizer.addEventListener('mouseleave', onSizerUp);
-// }
-
-// windowSizer.ondragstart = () => { return false; };
-
-// windowSizer.addEventListener('mousedown', onSizerStart);
+            const rightEdge = windowSizerWrapper.getBoundingClientRect().left;
 
 
-// windowSizer.onmousedown = (e) => {
-//     clientX = e.clientX;
-//     console.log(e);
-//     offsetX = e.offsetX;
-//     windowSizer.addEventListener('mousemove', moveAt);
+            const widthDiff = windowSizerWrapper.getBoundingClientRect().width - windowSizer.getBoundingClientRect().width;
 
-//     windowSizer.onmouseup = (e) => {
-//         windowSizer.removeEventListener('mousemove', moveAt);
-//         windowSizer.onmouseup = null;
-//     }
-// }
-
-
-windowSizer.onmousedown = function (event) {
-    event.preventDefault(); // prevent selection start (browser action)
-
-    let shiftX = event.clientX - windowSizer.getBoundingClientRect().left;
-    // shiftY not needed, the thumb moves only horizontally
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-
-    function onMouseMove(event) {
-        let newLeft = event.clientX - shiftX - windowSizerWrapper.getBoundingClientRect().left;
-
-        // the pointer is out of windowSizerWrapper => lock the thumb within the bounaries
-        if (newLeft < 0) {
-            newLeft = 0;
+            if (windowSizer.getBoundingClientRect().left < rightEdge) {
+                windowSizer.style.left = 0 + 'px';
+                console.log(rightEdge);
+                windowSizer.style.width = windowSizerWrapper.getBoundingClientRect().width - widthDiff + 'px';
+            }
         }
-        let rightEdge = windowSizerWrapper.offsetWidth - windowSizer.offsetWidth;
-        if (newLeft > rightEdge) {
-            newLeft = rightEdge;
-        }
-
-        windowSizer.style.left = newLeft + 'px';
     }
 
-    function onMouseUp() {
+    const onMouseUp = () => {
         document.removeEventListener('mouseup', onMouseUp);
         document.removeEventListener('mousemove', onMouseMove);
     }
 
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+
 };
 
-windowSizer.ondragstart = function () {
-    return false;
-};
+windowSizer.ondragstart = () => {return false;};
